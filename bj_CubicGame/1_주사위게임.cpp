82044b0b2191ece571865824cb4b06373b517f85
jeul2 <yoonjeyul@gmail.com>
2018-02-26 23:27:25 +0900

    배열에서 사이즈보다 큰 인덱스로 접근할때 동적할당과 배열선언과의 차이?

--- bj_CubicGame/1_주사위게임.cpp
+++ bj_CubicGame/1_주사위게임.cpp
@@ -17,12 +17,105 @@ g_ar [M] 배열 //wc 1000 -> 큐 사용
 3. 해당 위치의 값이 0이 아니면 2.를 호출하는 하고, 카운트 하는 함수.
 	0이면 카운트만하는 함수.
 4. 현재 위치가 도착점인지 체크하는 함수.
+
 반복문으로
+제한 : 전역변수 사용금지. 동적할당 사용.new delete 이용.
+
+결과1. 런타임에러 : 왜? M이 맥스를 초과할때 잘못된 접근. X
+					왜? N칸에 도착했을 때와 그 칸을 넘는 경우도 도착한 것이다. 문제 잘 안읽었음? ->wc 999칸에 +999가 적힌경우. O
+
+런타임에러는 동적할당을 사용함으로써 발생된 문제.
+Q. 동적할당 -> 가상메모리의 힙영역. 지역변수 -> 가상메모리의 스택영역.이라고 알고있다.
+	그렇다면 []로 사이즈를 초과하게 접근했으니 동적할당은 오류가 뜬다.
+	지역변수로 선언했을때는?오류가 안뜰까?
+	생각대로라면 안 뜰 것같다. 안뜬다면 왜 이런 차이가 날까?
+
+
 */
 #include <stdio.h>
 
+void fnRollCubic(const int *pSrcBoard, int nCurPosition, int nValue, int *pResPosition);
+int fnIsOrderChk(const int *pSrcBoard, int nCurPosition);
+int fnIsNotEndPoint(int nCurPosition, int nBoardSize);
+
 int main()
 {
+	int nData(0);
+	int nBoardSize(0); int nCubicMaxCount(0);
+	int *parBoard(nullptr);
+	int *parCubic(nullptr);
 
+	int nCurrentIndex(0);
+	int nCurrentCubicValue(0);
+	int nCountOfCubic(0);
+	
+	//
+	freopen("input.txt", "r", stdin);
+	scanf("%d %d", &nBoardSize, &nCubicMaxCount);
+
+	parBoard = new int[nBoardSize + 1 + 999]{};
+	parCubic = new int[nCubicMaxCount] {};
+
+	for (int i = 1; i <= nBoardSize; ++i)
+	{
+		scanf("%d", &nData);
+		parBoard[i] = nData;
+	}
+	for (int i = 0; i < nCubicMaxCount; ++i)
+	{
+		scanf("%d", &nData);
+		parCubic[i] = nData;
+	}
+	////
+
+	nCurrentIndex = 1; nCountOfCubic = 0;
+	nCurrentCubicValue = parCubic[nCountOfCubic];
+	while (fnIsNotEndPoint(nCurrentIndex, nBoardSize))
+	{
+		fnRollCubic(parBoard, nCurrentIndex, nCurrentCubicValue, &nCurrentIndex);
+
+		++nCountOfCubic;
+		if (nCountOfCubic < nCubicMaxCount)//
+			nCurrentCubicValue = parCubic[nCountOfCubic];
+	}
+
+	printf("%d\n", nCountOfCubic);
+
+	////
+	delete[] parBoard;
+	parBoard = nullptr;
+	delete[] parCubic;
+	parCubic = nullptr;
 	return 0;
+}
+
+void fnRollCubic(const int * pSrcBoard, int nCurPosition, int nValue, int * pResPosition)
+{
+	int nIndex(0);
+	int nTempValue(0);
+
+	nIndex = nCurPosition + nValue;
+	if (fnIsOrderChk(pSrcBoard, nIndex))
+	{
+		nTempValue = pSrcBoard[nIndex];
+		nIndex += nTempValue;
+	}
+
+	*pResPosition = nIndex;
+}
+
+int fnIsOrderChk(const int * pSrcBoard, int nCurPosition)
+{
+	if (pSrcBoard[nCurPosition] == 0)
+		return 0;
+	else
+		return 1;
+}
+
+int fnIsNotEndPoint(int nCurPosition, int nBoardSize)
+{
+	if (nCurPosition < nBoardSize)
+		return 1;
+	else
+		return 0;
 }
